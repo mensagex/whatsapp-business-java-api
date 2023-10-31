21fbbdceb8fd7201e76c0b2f14f1a979ed6dc799
brppe <brppe@DESKTOP-M78USCV>
2023-10-31 08:25:09 -0300

    ignore json fields

--- src/main/java/com/whatsapp/api/domain/messages/response/Message.java
+++ src/main/java/com/whatsapp/api/domain/messages/response/Message.java
@@ -1,11 +1,13 @@
 package com.whatsapp.api.domain.messages.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * The type Message.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record Message(
 
