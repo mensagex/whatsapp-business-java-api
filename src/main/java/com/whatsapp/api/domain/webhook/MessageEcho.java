93983304a47f209972ad5696624fc86ecfc48a32
brunopedrosa <bpedrosa@gmail.com>
2025-04-26 13:32:46 -0300

    add fields for various media types and properties to MessageEcho record

--- src/main/java/com/whatsapp/api/domain/webhook/MessageEcho.java
+++ src/main/java/com/whatsapp/api/domain/webhook/MessageEcho.java
@@ -4,14 +4,37 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.whatsapp.api.domain.messages.type.MessageType;
 
+import java.util.List;
+
+/**
+ * Igual ao {@link Message}, mas incluindo o campo "to" e marcando para ignorar
+ * propriedades desconhecidas, permitindo qualquer tipo de mídia em echoes.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record MessageEcho(
 
-        @JsonProperty("from") String from,
-        @JsonProperty("to") String to,
-        @JsonProperty("id") String id,
-        @JsonProperty("timestamp") String timestamp,
-        @JsonProperty("text") Text text,
-        @JsonProperty("type") MessageType type
+        @JsonProperty("reaction")    Reaction reaction,
+        @JsonProperty("image")       Image image,
+        @JsonProperty("sticker")     Sticker sticker,
+        @JsonProperty("location")    Location location,
+        @JsonProperty("contacts")    List<Contact> contacts,
+        @JsonProperty("button")      Button button,
+        @JsonProperty("context")     Context context,
 
-) { }
+        @JsonProperty("from")        String from,
+        @JsonProperty("to")          String to,
+
+        @JsonProperty("id")          String id,
+        @JsonProperty("text")        Text text,
+        @JsonProperty("errors")      List<Error> errors,
+        @JsonProperty("type")        MessageType type,
+        @JsonProperty("interactive") Interactive interactive,
+        @JsonProperty("timestamp")   String timestamp,
+        @JsonProperty("referral")    Referral referral,
+        @JsonProperty("order")       Order order,
+        @JsonProperty("system")      System system,
+        @JsonProperty("video")       Video video,
+        @JsonProperty("audio")       Audio audio,
+        @JsonProperty("document")    Document document
+
+) {}
