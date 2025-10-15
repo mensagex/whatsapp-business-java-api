8109c8008fd1858d4d4b47836a84b01d1f398513
Bruno <42983263+brunopedrosa@users.noreply.github.com>
2025-10-15 09:58:34 -0300

    Add url to webhook image payload

--- src/main/java/com/whatsapp/api/domain/webhook/Image.java
+++ src/main/java/com/whatsapp/api/domain/webhook/Image.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @param mimeType The mime type of the media.
  * @param caption  Added to Webhooks if it has been previously specified.                 The caption that describes the media.
  * @param id       The ID of the medi
+ * @param url      URL for the media, if the message payload includes it
  */
 public record Image(
 
@@ -18,6 +19,8 @@ public record Image(
 
         @JsonProperty("caption") String caption,
 
-        @JsonProperty("id") String id) {
+        @JsonProperty("id") String id,
+
+        @JsonProperty("url") String url) {
 
 }
